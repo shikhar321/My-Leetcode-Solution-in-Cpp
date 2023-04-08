526b4b71866911875a98f18d22c7307777b142db
Shikhar Gupta <guptashikhar321@gmail.com>
2023-04-09 02:43:57 +0530

    Time: 64 ms (56.24%), Space: 32.6 MB (54.92%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        vector<vector<int>> answer;
+        vector<int> v1;
+        vector<int> v2;
+        unordered_map<int, int> mp1;
+        unordered_map<int, int> mp2;
+        for(int i = 0; i<nums1.size(); i++) {
+            mp1[nums1[i]]++;
+        }
+        for(int i = 0; i<nums2.size(); i++) {
+            mp2[nums2[i]]++;
+        }
+        for(auto it: mp1) {
+            if(mp2.find(it.first) == mp2.end()) {
+                v1.push_back(it.first);
+            }
+        }
+        for(auto it: mp2) {
+            if(mp1.find(it.first) == mp1.end()) {
+                v2.push_back(it.first);
+            }
+        }
+        answer.push_back(v1);
+        answer.push_back(v2);
+        return answer;
+    }
+};
