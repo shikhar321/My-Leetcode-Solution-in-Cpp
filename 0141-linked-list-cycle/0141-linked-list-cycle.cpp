3c3f0341ebbab4e9c1f0c0b58a9c9adb0350e015
Shikhar Gupta <guptashikhar321@gmail.com>
2023-04-24 19:26:22 +0530

    Time: 11 ms (64.09%), Space: 8.2 MB (48.90%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,14 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        int count = 1;
-        if(head == NULL) {
-            return false;
-        }
-        while(head->next != NULL) {
-            count++;
-            head = head->next;
-            if(count>10001) {
+        // The trick is that slow and fast pointers always meet when there is a cycle
+        // in a linked list
+        if(head == NULL || head->next == NULL)  return false;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow) {
                 return true;
             }
         }
