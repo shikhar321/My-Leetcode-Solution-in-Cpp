13d4ea22e165adba625c21c9a51da7a699bc3404
Shikhar Gupta <guptashikhar321@gmail.com>
2023-05-31 18:09:52 +0530

    Time: 25 ms (39.17%), Space: 46.6 MB (37.26%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        int ans = money - prices[0] - prices[1];
+        if(ans >= 0) {
+            return ans;
+        }
+        return money;
+    }
+};
