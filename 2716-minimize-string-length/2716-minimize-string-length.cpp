59536a15c16a107fef4eb2a0cb45f464325d91a6
Shikhar Gupta <guptashikhar321@gmail.com>
2023-06-17 19:50:59 +0530

    Time: 44 ms (41.83%), Space: 13.2 MB (43.34%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minimizedStringLength(string s) {
+        set<int> st;
+        for(int i = 0; i<s.size(); i++) {
+            st.insert(s[i]);
+        }
+        return st.size();
+    }
+};
